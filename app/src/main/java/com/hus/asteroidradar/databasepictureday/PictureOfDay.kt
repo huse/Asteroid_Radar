0ef3aee77e1675d7f34e17f5891d4d8447613aee
Hans <hus.kpr@gmail.com>
2021-03-16 12:35:14 -0700

    resolved issue for  Picture of the day

--- app/src/main/java/com/hus/asteroidradar/databasepictureday/PictureOfDay.kt
+++ app/src/main/java/com/hus/asteroidradar/databasepictureday/PictureOfDay.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.JsonClass
 import kotlinx.android.parcel.Parcelize
 @JsonClass(generateAdapter = true)
 @Entity
-data class PictureOfDay(@PrimaryKey(autoGenerate = true) val uid: Long,
+data class PictureOfDay(@PrimaryKey(autoGenerate = true) val uid: Long?,
                         @Json(name = "media_type") val mediaType: String?,
                         val title: String?,
                         val url: String?){
--- app/src/main/java/com/hus/asteroidradar/databasepictureday/PictureOfDayDataBase.kt
+++ app/src/main/java/com/hus/asteroidradar/databasepictureday/PictureOfDayDataBase.kt
@@ -3,7 +3,7 @@ package com.hus.asteroidradar.databasepictureday
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [PictureOfDay::class], version = 4)
+@Database(entities = [PictureOfDay::class], version = 5)
 abstract class PictureOfDayDataBase : RoomDatabase() {
     abstract fun pictureDayDatabase(): PictureOfDayDao
 }
